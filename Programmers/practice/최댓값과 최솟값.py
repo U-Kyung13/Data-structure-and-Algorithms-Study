1397984b8524ed40b629a8ecdeedcda3ba2521ca
YuKyung Kim <90254892+U-Kyung13@users.noreply.github.com>
2023-05-01 11:31:10 +0900

    Update 최댓값과 최솟값.py

--- Programmers/practice/최댓값과 최솟값.py
+++ Programmers/practice/최댓값과 최솟값.py
@@ -2,3 +2,7 @@ def solution(s):
     s_int = [int(i) for i in s.split(' ')] 
     answer = str(min(s_int)) + ' ' + str(max(s_int))
     return answer
+
+
+# split()의 default는 공백이다 -> s.split()이라고 써도 됨
+# list comprehension 대신 list(map(int, s.split()))으로 할 수 있음
