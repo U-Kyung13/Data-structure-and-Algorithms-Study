11ac0f1019510e378e0369a43ae15acac2192583
YuKyung Kim <90254892+U-Kyung13@users.noreply.github.com>
2023-05-06 15:45:20 +0900

    Update 0506_숫자의표현.py

--- Programmers/practice/0506_숫자의표현.py
+++ Programmers/practice/0506_숫자의표현.py
@@ -1,4 +1,4 @@
-# 내 풀이 : 투 포인터
+# 투 포인터
 def solution(n):
     n_list = [i for i in range(1, n+1)]
     answer = 0
@@ -15,17 +15,4 @@ def solution(n):
         elif target > n:
             left += 1
             right = left + 1
-    return answer
-
-
-# 다른 풀이 
-def solution(n):
-    anwer = 0 
-    for i in range(1, n+1):
-        sum_n = 0 
-        while sum_n < n:
-            sum_n += i 
-            i += 1
-        if sum_n == n:
-            answer += 1
     return answer
