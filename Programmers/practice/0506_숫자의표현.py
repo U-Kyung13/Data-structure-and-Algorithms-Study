87ecce2c240c0daabb0f710c528774c149465551
YuKyung Kim <90254892+U-Kyung13@users.noreply.github.com>
2023-05-06 15:28:45 +0900

    Create 0506_숫자의표현.py

---
+++ Programmers/practice/0506_숫자의표현.py
@@ -0,0 +1,17 @@
+def solution(n):
+    n_list = [i for i in range(1, n+1)]
+    answer = 0
+    left = 0
+    right = 1
+    while left < n:
+        target = sum(n_list[left:right+1])
+        if target < n:
+            right += 1
+        elif target == n:
+            answer += 1
+            left += 1
+            right = left + 1
+        elif target > n:
+            left += 1
+            right = left + 1
+    return answer
