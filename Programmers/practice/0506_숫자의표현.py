7b3215a6f4cd04d64ff5b7caabdd4233f3157bd6
YuKyung Kim <90254892+U-Kyung13@users.noreply.github.com>
2023-05-06 15:42:24 +0900

    Update 0506_숫자의표현.py

--- Programmers/practice/0506_숫자의표현.py
+++ Programmers/practice/0506_숫자의표현.py
@@ -16,3 +16,16 @@ def solution(n):
             left += 1
             right = left + 1
     return answer
+
+
+# 다른 풀이 
+def solution(n):
+    anwer = 0 
+    for i in range(1, n+1):
+        sum_n = 0 
+        while sum_n < n:
+            sum_n += i 
+            i += 1
+        if sum_n == n:
+            answer += 1
+    return answer
